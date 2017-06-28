aee001a7e3cb2314787ae7d8b13b1d7a19c58da1
Hongbo He <hhbcarl@gmail.com>
2017-06-28 22:16:12 +0800

    More test

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
@@ -8,3 +9,17 @@ class PostModelTest(TestCase):
     def test_render_markdown(self):
         p = Post(content='aa')
         self.assertEqual(p.html_content, '<p>aa</p>\n')
+
+
+class PostViewTest(TestCase):
+
+    def test_post_view(self):
+        p = Post(
+            slug='first-blog',
+            title='First blog',
+            content='I am hhb',
+        )
+        p.save()
+        url = reverse('post', args=(p.slug,))
+        resp = self.client.get(url)
+        self.assertContains(resp, 'I am hhb')
