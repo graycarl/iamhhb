6d7f03e8362990917dca342669936f13fac60358
Hongbo He <hhbcarl@gmail.com>
2017-07-22 18:27:31 +0800

    Add to heroku

--- iamhhb/settings.py
+++ iamhhb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,7 +81,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'var', 'db.sqlite3'),
     }
 }
-
+# For heroku
+DATABASES['default'].update(dj_database_url.config(conn_max_age=50))
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
