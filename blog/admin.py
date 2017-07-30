9bcf5633d9bbcd82024077f7656fb263b24c780d
Hongbo He <hhbcarl@gmail.com>
2017-07-30 14:58:20 +0800

    Update post admin. fix #16

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'summary', 'created_at')
+
+
+admin.site.register(Post, PostAdmin)
